21d1806e45a42c51f5e67ff9378261402f418707
ayush8116-tech <vermajiii8116@gmail.com>
2025-09-19 13:57:25 +0530

    Created file for finding ap with strings

---
+++ ap_with_strings.js
@@ -0,0 +1,13 @@
+const difference = "..."
+let firstTerms = "...";
+let term ="";
+const lastTerm = ".............................."
+
+while(term.length<lastTerm.length){
+
+    console.log(firstTerms.length);
+    firstTerms = firstTerms + difference;
+     term = term + ".";
+}
+
+
