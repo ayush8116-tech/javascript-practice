2e3c9f65264318d94c7a8948e57df050fdd3cea7
ayush8116-tech <vermajiii8116@gmail.com>
2025-09-19 13:55:24 +0530

    arithmetic progression by using numbers

---
+++ arithmetic_progression.js
@@ -0,0 +1,11 @@
+const firstNumber = 6;
+const difference = 2;
+const numOfTerms = 7;
+let valueOfTerm ;
+let countCheck = true;
+
+for(let countFactor = 0 ; countFactor < numOfTerms ; countFactor++){
+
+         valueOfTerm = firstNumber + (countFactor * difference);
+        console.log(valueOfTerm);
+}
