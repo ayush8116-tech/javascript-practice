dda4cf939298fdb160882f7f9270acd53a72b9b1
ayush8116-tech <vermajiii8116@gmail.com>
2025-09-20 13:26:23 +0530

    Update lion_hunt_assignment.js

--- lion_hunt_assignment.js
+++ lion_hunt_assignment.js
@@ -1,9 +1,15 @@
 const inputTestCase = "L    Z" ;
 let indexInputTestCase = 0;
+let spaceCount = 1;
+while (inputTestCase[indexInputTestCase] !== "Z") {
+    
+    if(inputTestCase[indexInputTestCase] === "L"){
+        indexInputTestCase = indexInputTestCase + 1;
+    }
 
-if(inputTestCase[indexInputTestCase] === "L"){
-    indexInputTestCase = indexInputTestCase + 1;
 }
+console.log(spaceCount);
+
 console.log(indexInputTestCase);
 
 console.log(inputTestCase.length);
