2fded370661560a02277872fda9872276b439a4d
ayush8116-tech <vermajiii8116@gmail.com>
2025-09-20 13:55:58 +0530

    if statement for checking consecutive l

--- lion_hunt_assignment.js
+++ lion_hunt_assignment.js
@@ -1,8 +1,18 @@
-const inputTestCase = "L    Z" ;
+const inputTestCase = " LZ" ;
 let spaceCount = 0;
 
-let indexInputTestCase = 0;
-while (inputTestCase[indexInputTestCase] !== "Z") {
+let indexInputTestCase = 0;    //change it when checking of l to l is done
+
+
+if(inputTestCase[indexInputTestCase] === "L"){
+    
+   
+}
+
+
+
+// indexInputTestCase = 0;
+while (inputTestCase[indexInputTestCase] !== "Z") {                        //different category for counting space
   
     if(inputTestCase[indexInputTestCase] === " "){
         spaceCount = spaceCount + 1;
@@ -14,7 +24,6 @@ while (inputTestCase[indexInputTestCase] !== "Z") {
 }
 console.log(spaceCount);
 
-console.log(indexInputTestCase);
+// console.log(indexInputTestCase);
 
-console.log(inputTestCase.length);
- 
+// console.log(inputTestCase.length);
