ae1b36b2718f302c5fe4c68757f8f9a6389f7543
ayush8116-tech <vermajiii8116@gmail.com>
2025-09-21 15:06:45 +0530

    Update atm_simulation.js

--- atm_simulation.js
+++ atm_simulation.js
@@ -2,10 +2,14 @@
 let balance = 1000;
 const deposit =  100;
 const withdrawal = 100;
-const input = "1";
+const input = "2";
+let balanceAfterDeposit = balance + deposit;
 switch(input){
     case "1": console.log(balance);
-    break;       
+    break;   
+    case "2": console.log(balanceAfterDeposit);
+    break;  
+    
 }
 
 balance = balance - withdrawal;
