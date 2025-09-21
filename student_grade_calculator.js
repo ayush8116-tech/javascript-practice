cf210445c8c8272236fa1ae25ea5df241d38da74
ayush8116-tech <vermajiii8116@gmail.com>
2025-09-21 13:52:22 +0530

    Updated to add some boolean value for invalid cases

--- student_grade_calculator.js
+++ student_grade_calculator.js
@@ -1,6 +1,6 @@
 const subject1 = 90;
-const subject2 = 1;
-const subject3 = 10;
+const subject2 = 30;
+const subject3 = 100;
 
 const maxLimit = 100;
 const minLimit = 0;
@@ -8,14 +8,15 @@ const minLimit = 0;
 let sum = subject1 + subject2 + subject3;
 let average = sum / 3 ;
 
-let error;
+let isInvalid = false;
 
 if(subject1 > maxLimit || subject2 > maxLimit || subject3 > maxLimit || subject1 < minLimit || subject2 < minLimit || subject3 < minLimit ){
-    error = "Invalid marks!! (marks should be less than 100 or greater than 0)"
-    console.log(error);    
+    const errorMessage = "Invalid marks!! (marks should be less than 100 or greater than 0)"
+    console.log(errorMessage);   
+    isInvalid = true; 
 }
 
-if(error !== "Invalid marks!! (marks should be less than 100 or greater than 0)"){
+if(isInvalid === false){
 
     if(average>=90){
         console.log("A");
