d862b50f6896583234e4bb2666bfef52ba7399ec
ayush8116-tech <vermajiii8116@gmail.com>
2025-09-21 18:44:05 +0530

    Update lion_hunt_assignment.js

--- lion_hunt_assignment.js
+++ lion_hunt_assignment.js
@@ -1,31 +1,47 @@
-const inputTestCase = "L     L   Z" ;
+const inputTestCase = "L ZL" ;
 let spaceCount = 0;
 
-let indexInputTestCase = 0;    //change it when checking of l to l is done
+let indexInputTestCase = 0;    
 
+let distanceFromLionToZebra =0;
 
-if(inputTestCase[indexInputTestCase] === "L"){
+let firstReferenceIndex;
+console.log("hello");
+
+while ( inputTestCase[indexInputTestCase] !== "Z"  ) {  
+
+    if(inputTestCase[indexInputTestCase] === "L"){
+        firstReferenceIndex = indexInputTestCase;
+    }
     indexInputTestCase = indexInputTestCase + 1;
-    while (inputTestCase[indexInputTestCase] !== "L" ) {
-        indexInputTestCase = indexInputTestCase + 1;
+
+    if(inputTestCase[indexInputTestCase] === "Z"){
+        distanceFromLionToZebra = indexInputTestCase - firstReferenceIndex - 1 ;
+    } 
+ }
+ let distanceFromZebraToLion =0;
+
+while ( inputTestCase[indexInputTestCase] !== "L" ) {  
+
+    if(inputTestCase[indexInputTestCase] === "Z"){
+        firstReferenceIndex = indexInputTestCase;
     }
-   
-}
+    indexInputTestCase = indexInputTestCase + 1;
 
-
-
-// indexInputTestCase = 0;
-while (inputTestCase[indexInputTestCase] !== "Z") {                        //different category for counting space
-  
-    if(inputTestCase[indexInputTestCase] === " "){
-        spaceCount = spaceCount + 1;
+    if(inputTestCase[indexInputTestCase] === "L"){
+        distanceFromZebraToLion = indexInputTestCase - firstReferenceIndex - 1 ;
     }
-
-        indexInputTestCase = indexInputTestCase + 1;
+        
     
-
 }
-console.log(spaceCount);
+
+const minimumDistanceBetweenLionAndZebra = (distanceFromLionToZebra < distanceFromZebraToLion)? distanceFromLionToZebra : distanceFromZebraToLion;
+console.log(distanceFromLionToZebra, distanceFromZebraToLion);
+console.log("Nearest hunt distance of Lion is ", minimumDistanceBetweenLionAndZebra);
+
+
+
+
 
 // console.log(indexInputTestCase);
 
