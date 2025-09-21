a254a5e592e058c2e78bbe8983ef2eb52a8d35a1
ayush8116-tech <vermajiii8116@gmail.com>
2025-09-21 13:17:19 +0530

    Updated to print invalid marks if marks are greater than 100

--- student_grade_calculator.js
+++ student_grade_calculator.js
@@ -1,18 +1,23 @@
 const subject1 = 90;
 const subject2 = 70;
-const subject3 = 98;
+const subject3 = 101;
+
+const maxLimit = 100;
 
 let sum = subject1 + subject2 + subject3;
 let average = sum / 3 ;
 
 console.log(average);
 
+if(subject1 > maxLimit || subject2 > maxLimit || subject3 > maxLimit ){
+    console.log("Invalid marks!! (marks should be less than or equal to 100)");    
+}
+
 if(average>=90){
     console.log("A");
 }
 if(average>=75 && average < 90){
-    console.log("B");
-    
+    console.log("B");   
 }
 if(average>=50 && average < 75){
     console.log("C"); 
