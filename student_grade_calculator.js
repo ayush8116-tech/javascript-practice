b26e94efec66ef09e8a4d380a65e63311a1eda6b
ayush8116-tech <vermajiii8116@gmail.com>
2025-09-21 13:41:11 +0530

    Update student_grade_calculator.js

--- student_grade_calculator.js
+++ student_grade_calculator.js
@@ -1,18 +1,20 @@
 const subject1 = 90;
-const subject2 = 70;
-const subject3 = 101;
+const subject2 = 1;
+const subject3 = 10;
 
 const maxLimit = 100;
+const minLimit = 0;
 
 let sum = subject1 + subject2 + subject3;
 let average = sum / 3 ;
 
-console.log(average);
 let error;
+
 if(subject1 > maxLimit || subject2 > maxLimit || subject3 > maxLimit || subject1 < minLimit || subject2 < minLimit || subject3 < minLimit ){
     error = "Invalid marks!! (marks should be less than 100 or greater than 0)"
     console.log(error);    
 }
+
 if(error !== "Invalid marks!! (marks should be less than 100 or greater than 0)"){
 
     if(average>=90){
